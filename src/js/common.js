b4892b6170440e8e1b93fe8ed8a926dfd7f3c781
webkey <serhiy_i@ukr.net>
2017-02-23 20:27:23 +0300

    office_20170223_2030

--- src/js/common.js
+++ src/js/common.js
@@ -771,7 +771,7 @@ function masonryInit() {
 		var self = this,
 			$buttonMenu = self.$btnMenu;
 
-		$buttonMenu.on('click', function (e) {
+		$buttonMenu.on('mousedown touchstart vmousedown', function (e) {
 			e.preventDefault();
 
 			if (self.navIsOpened) {
@@ -1049,6 +1049,69 @@ function fileInput() {
 }
 /*file input end end*/
 
+/*breadcrumbs add hover class*/
+function breadcrumbsBehavior(){
+	var $breadcrumbsItemHasDrop = $('.breadcrumbs__item_has-drop');
+	if (!DESKTOP) {
+
+		for(var i = 0; i < $breadcrumbsItemHasDrop.length;i++){
+			var $this = $breadcrumbsItemHasDrop.eq(i);
+			$this.attr('data-text', $this.children('a').children('span').text());
+		}
+
+		$breadcrumbsItemHasDrop
+			.not(':last-child')
+			.children('a')
+			.children('span')
+			.text('...');
+
+		$breadcrumbsItemHasDrop.on('click', function (e) {
+			var $breadcrumbsItemCurrent = $(this);
+			if ($breadcrumbsItemCurrent.hasClass('hover')){
+				return;
+			}
+			e.stopPropagation();
+
+			$breadcrumbsItemHasDrop
+				.removeClass('hover breadcrumbs__item_long')
+				.addClass('breadcrumbs__item_short');
+
+			$breadcrumbsItemHasDrop.children('a').children('span').text('...');
+
+			$breadcrumbsItemCurrent
+				.addClass('hover breadcrumbs__item_long')
+				.removeClass('breadcrumbs__item_short');
+
+			$breadcrumbsItemCurrent.children('a').children('span').text($breadcrumbsItemCurrent.data('text'));
+
+			e.preventDefault();
+		});
+
+		$('.breadcrumbs-drop').on('click', function (e) {
+			e.stopPropagation();
+		});
+
+		$(document).on('click', function () {
+			$('.breadcrumbs__item_has-drop').removeClass('hover');
+		});
+
+	} else {
+		$breadcrumbsItemHasDrop.on('mouseenter', function () {
+			$breadcrumbsItemHasDrop
+				.removeClass('hover breadcrumbs__item_long')
+				.addClass('breadcrumbs__item_short');
+
+			$(this)
+				.addClass('hover breadcrumbs__item_long')
+				.removeClass('breadcrumbs__item_short');
+
+		}).on('mouseleave', function () {
+			$(this).removeClass('hover');
+		});
+	}
+}
+/*breadcrumbs add hover class end*/
+
 /**
  * !footer at bottom
  * */
@@ -1146,6 +1209,7 @@ $(document).ready(function(){
 	masonryInit();
 	siteMapPopup();
 	fileInput();
+	breadcrumbsBehavior();
 	footerBottom();
 	formSuccessExample();
 });
